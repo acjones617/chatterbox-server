907251944ec128086f2423da24142fdb2a84a721
Hack Reactor Students <acjones617@gmail.com>
2014-05-05 21:49:43 -0700

    Fixes roomname foldering system

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,8 @@
+/* Import node's url module: */
+var url = require("url");
+
+var _ = require("underscore");
+
 /* You should implement your request handler function in this file.
  * And hey! This is already getting passed to http.createServer()
  * in basic-server.js. But it won't work as is.
@@ -21,50 +26,66 @@ var handleRequest = function(request, response) {
   console.log("Serving request type " + request.method + " for url " + request.url);
   // console.log(Object.keys(request));
   // console.log(Object.keys(request.headers['user-agent']));
-  var statusCode = 200;
 
+  var urlData = url.parse(request.url);
+  var roomname = urlData.pathname.split('/')[2];
+  var dataToSend;
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
   var headers = defaultCorsHeaders;
+  var statusCode = 200;
 
   headers['Content-Type'] = "text/plain";
-
-
-
-  // interpret request
-  if (request.method === "GET") {
-    console.log('get me some messages!'); // #todo
-    /* .writeHead() tells our server what HTTP status code to send back */
-    response.writeHead(statusCode, headers);
-
-    /* Make sure to always call response.end() - Node will not send
-     * anything back to the client until you do. The string you pass to
-     * response.end() will be the body of the response - i.e. what shows
-     * up in the browser.*/
-    response.end(JSON.stringify(dataStore));
-    // return message s
-  } else if (request.method === "POST") {
-    request.on('data', function(data) {
-      // data =??? <Buffer 7b 22 75 73 65 72 6e 61 6d 65 22 3a 22 68 61 74 73 6f 6e 63 61 74 73 22 2c 22 74 65 78 74 22 3a 22 65 68 3f 22 2c 22 72 6f 6f 6d 6e 61 6d 65 22 3a 22 6c ...>
-      // create a data store for messages sent in
-      // create an object & parse post input into object properties
-      var newMessage = JSON.parse(data.toString());
-      newMessage.createdAt = new Date();
-      newMessage.updatedAt = new Date();
-      newMessage.objectId = counter;
-      // push the object into dataStore.results
-      // dataStore.results.push(newMessage);
-      dataStore.results.unshift(newMessage);
-      counter++;
-      statusCode = 201;
+  if (urlData.pathname.split('/')[1] !== "classes") {
+    response.writeHead(404, headers);
+    response.end("{}");
+  } else {
+    // interpret request
+    if (request.method === "GET") {
+      /* .writeHead() tells our server what HTTP status code to send back */
+      statusCode = 200;
       response.writeHead(statusCode, headers);
-      response.end("Messages posted successfully");
-    });
+
+      /* Make sure to always call response.end() - Node will not send
+       * anything back to the client until you do. The string you pass to
+       * response.end() will be the body of the response - i.e. what shows
+       * up in the browser.*/
+
+      // send back messages that are in the roomname we are in.
+      dataToSend = _.filter(dataStore.results, function(message) {
+        return message.roomname === roomname;
+      });
+
+      response.end(JSON.stringify({results: dataToSend}));
+      // return message s
+    } else if (request.method === "POST") {
+      request.on('data', function(data) {
+        // data =??? <Buffer 7b 22 75 73 65 72 6e 61 6d 65 22 3a 22 68 61 74 73 6f 6e 63 61 74 73 22 2c 22 74 65 78 74 22 3a 22 65 68 3f 22 2c 22 72 6f 6f 6d 6e 61 6d 65 22 3a 22 6c ...>
+        // create a data store for messages sent in
+        // create an object & parse post input into object properties
+        var newMessage = JSON.parse(data.toString());
+        newMessage.createdAt = new Date();
+        newMessage.updatedAt = new Date();
+        newMessage.objectId = counter;
+        newMessage.roomname = roomname;
+        // push the object into dataStore.results
+        // dataStore.results.push(newMessage);
+        dataStore.results.unshift(newMessage);
+        dataToSend = {results: [newMessage]};
+        counter++;
+        statusCode = 201;
+        response.writeHead(statusCode, headers);
+        response.end(JSON.stringify(dataToSend));
+      });
+    } else if (request.method === "OPTIONS") {
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(dataStore));
+    }
   }
 
-  response.writeHead(statusCode, headers);
-  response.end("Messages posted successfully");
+
+
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).

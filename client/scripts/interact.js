b25a5bda25d9eaf4c2502d0708dfee16ce05dc4e
Hack Reactor Students <acjones617@gmail.com>
2014-05-05 19:21:52 -0700

    Adds client code

---
+++ client/scripts/interact.js
@@ -0,0 +1,21 @@
+// make linter happy
+var $ = $;
+var app = app;
+// end
+
+// interact with messageList
+
+$(function(){
+  // Toggle friendship of a user when username is clicked.
+  $('body').on('click', '.username', function(event) {
+    event.preventDefault();
+    app.allUsers[$(this).text()] = !app.allUsers[$(this).text()];
+    app.renderRoom();
+    app.renderFriendList();
+  });
+
+  setInterval(function() {
+    app.fetch();
+  }, 2000);
+
+});
